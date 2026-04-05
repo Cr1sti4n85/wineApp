5fcc028173bea2bb8d88845052a67de7c2fb2a2e
Cristian Perez <cperezlecaros@gmail.com>
2026-04-04 21:41:47 -0300

    feat: set up swipe refresh

--- app/src/main/java/com/soracel/wineapp/MainActivity.kt
+++ app/src/main/java/com/soracel/wineapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.soracel.wineapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,12 +9,14 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.soracel.wineapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         setupAdapter()
         setupRecyclerView()
         setupRetrofit()
+        setupSwipeRefresh()
     }
 
     private fun setupAdapter() {
@@ -46,6 +50,13 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.apply {
             layoutManager = StaggeredGridLayoutManager(3, RecyclerView.VERTICAL)
             adapter = this@MainActivity.adapter
+        }
+    }
+
+    private fun setupSwipeRefresh(){
+        binding.srlWines.setOnRefreshListener {
+            adapter.submitList(listOf()) //limpia la lista
+            getWines()
         }
     }
 
@@ -60,57 +71,47 @@ class MainActivity : AppCompatActivity() {
 
     private fun getWines() {
         lifecycleScope.launch(Dispatchers.IO) {
-            val wines = service.getWines()
+            try {
+                val serverOK = Random.nextBoolean()
+                val wines = if(serverOK) service.getWines() else emptyList()
 
-            withContext(Dispatchers.Main){
-                adapter.submitList(wines)
+                withContext(Dispatchers.Main){
+                    if(wines.isNotEmpty()){
+                        showNoData(false)
+                        showRecyclerView(true)
+                        adapter.submitList(wines)
+                    } else {
+                        showRecyclerView(false)
+                        showNoData(true)
+                    }
+                }
+            } catch (e: Exception){
+                showMsg(R.string.common_string_failed)
+            } finally {
+                showProgress(false)
             }
         }
     }
 
-    private fun getLocalWines() = listOf(Wine(
-        "Maselva",
-        "Emporda 2012",
-        Rating("4.8", "540 ratings"),
-        "France",
-        "https://images.vivino.com/thumbs/nC9V6L2mQQSq0s-wZLcaxw_pb_x300.png",
-        1
-    ), Wine(
-        "Ernesto Ruffo",
-        "Carmenere",
-        Rating("4.8", "540 ratings"),
-        "Italy",
-        "https://images.vivino.com/thumbs/00o0kud1xybsc_375x500.jpg",
-        1
-    ), Wine(
-        "Cartuxa",
-        "The Beast Cabernet Sauvignon 2012",
-        Rating("4.8", "540 ratings"),
-        "Spain",
-        "https://images.vivino.com/thumbs/nC9V6L2mQQSq0s-wZLcaxw_pb_x300.png",
-        1
-    ),
-        Wine(
-            "Sine Qua Non",
-            "Merlot",
-            Rating("4.8", "540 ratings"),
-            "Chile",
-            "https://images.vivino.com/thumbs/00o0kud1xybsc_375x500.jpg",
-            1
-        ),
-        Wine(
-            "Garbole",
-            "Hurlo 2009",
-            Rating("4.8", "540 ratings"),
-            "France",
-            "https://images.vivino.com/thumbs/nC9V6L2mQQSq0s-wZLcaxw_pb_x300.png",
-            1
-        )
+    private fun showRecyclerView(isVisible: Boolean){
+        binding.recyclerView.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
 
-    )
+    private fun showNoData(isVisible: Boolean){
+        binding.tvNoData.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+
+    private fun showProgress(isVisible: Boolean){
+        binding.srlWines.isRefreshing = isVisible
+    }
+
+    private fun showMsg(msgRes: Int){
+        Snackbar.make(binding.root, msgRes, Snackbar.LENGTH_SHORT).show()
+    }
 
     override fun onResume() {
         super.onResume()
+        showProgress(true)
         getWines()
     }
 }
